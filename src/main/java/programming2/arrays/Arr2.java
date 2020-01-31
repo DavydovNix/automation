284720eb4211c11841d62cfc1fd1dcf5b64d479f
DavydovNix <D30011921d>
2020-01-31 23:53:14 +0200

    Arrays: Task2

--- src/main/java/programming2/arrays/Arr2.java
+++ src/main/java/programming2/arrays/Arr2.java
@@ -11,7 +11,7 @@ public class Arr2 {
     3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке.
     Каждый элемент - с новой строки.*/
     public static void main(String[] args) throws IOException {
-        String[] array = new String[5];
+        String[] array = new String[10];
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < array.length - 2; i++) {
             array[i] = reader.readLine();
