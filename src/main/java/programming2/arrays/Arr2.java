6153dcd2d5b3a8b5d3546a547d41a5e62ce8160d
DavydovNix <D30011921d>
2020-01-31 23:47:18 +0200

    Arrays: Task2

--- src/main/java/programming2/arrays/Arr2.java
+++ src/main/java/programming2/arrays/Arr2.java
@@ -8,7 +8,8 @@ public class Arr2 {
     /*Массив из строчек в обратном порядке
     1. Создать массив на 10 строчек.
     2. Ввести с клавиатуры 8 строчек и сохранить их в массив.
-    3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке. Каждый элемент - с новой строки.*/
+    3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке.
+    Каждый элемент - с новой строки.*/
     public static void main(String[] args) throws IOException {
         String[] array = new String[5];
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
