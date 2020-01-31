da089ce97986352f9a02e2eee3d46c3a6bbe915b
DavydovNix <D30011921d>
2020-01-31 23:58:48 +0200

    Arrays: Task2

--- src/main/java/programming2/arrays/Arr3.java
+++ src/main/java/programming2/arrays/Arr3.java
@@ -1,4 +1,9 @@
 package programming2.arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*2 массива
   1. Создать массив на 10 строк.
   2. Создать массив на 10 чисел.
@@ -7,7 +12,16 @@ package programming2.arrays;
   индекс/номер ячейки которой совпадает с текущим индексом из массива чисел.
   5. Вывести содержимое массива чисел на экран, каждое значение выводить с новой строки.*/
 public class Arr3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String[] lines = new String[10];
+        int[] numbers = new int[10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = reader.readLine();
+            numbers[i] = lines[i].length();
+        }
+        for (int element:numbers) {
+            System.out.println(element);
+        }
     }
 }
