bd8c8f2fb6179a455cd64d8c45c1876d8d998b39
Denis <davydovdmail@gmail.com>
2020-05-17 22:11:31 +0300

    Code editing

--- src/main/java/programming2/arrays/Arr5.java
+++ src/main/java/programming2/arrays/Arr5.java
@@ -12,18 +12,18 @@ import java.io.InputStreamReader;
 5. Вывести второй маленький массив на экран, каждое значение выводить с новой строки.*/
 public class Arr5 {
     public static void main(String[] args) throws IOException {
-        int[] mainArray = new int[6];
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < mainArray.length; i++) {
-            String s = reader.readLine();
-            mainArray[i] = Integer.parseInt(s);
+        int[] big_array = new int[20];
+        BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+        for (int i = 0; i < big_array.length; i++) {
+            big_array[i] = Integer.parseInt(reader.readLine());
         }
-        int[] array1 = new int[3];
-        int[] array2 = new int[3];
-        System.arraycopy(mainArray,0,array1,0,array1.length);
-        System.arraycopy(mainArray,mainArray.length/2,array2,0,array2.length);
-        for (int element:array2) {
-            System.out.println(element);
+        int[] small_array1 = new int[10];
+        int[] small_array2 = new int[10];
+        System.arraycopy(big_array,0,small_array1,0,small_array1.length);
+        System.arraycopy(big_array,big_array.length/2,small_array2,0,small_array2.length);
+        System.out.println("Second small array:");
+        for (int i = 0; i < small_array2.length; i++) {
+            System.out.println(small_array2[i]);
         }
     }
 }
