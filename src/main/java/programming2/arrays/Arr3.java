9bbb4267b46da0c3ff820e9c1730992f06f7ee53
Denis <davydovdmail@gmail.com>
2020-05-17 17:51:54 +0300

    Code editing

--- src/main/java/programming2/arrays/Arr3.java
+++ src/main/java/programming2/arrays/Arr3.java
@@ -4,23 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/*2 массива
-  1. Создать массив на 10 строк.
-  2. Создать массив на 10 чисел.
-  3. Ввести с клавиатуры 10 строк, заполнить ими массив строк.
-  4. В каждую ячейку массива чисел записать длину строки из массива строк,
-  индекс/номер ячейки которой совпадает с текущим индексом из массива чисел.
-  5. Вывести содержимое массива чисел на экран, каждое значение выводить с новой строки.*/
 public class Arr3 {
+    /*2 массива
+1. Создать массив на 10 строк.
+2. Создать массив на 10 чисел.
+3. Ввести с клавиатуры 10 строк, заполнить ими массив строк.
+4. В каждую ячейку массива чисел записать длину строки из массива строк,
+индекс/номер ячейки которой совпадает с текущим индексом из массива чисел.
+5. Вывести содержимое массива чисел на экран, каждое значение выводить с новой строки.*/
     public static void main(String[] args) throws IOException {
-        String[] lines = new String[10];
-        int[] numbers = new int[10];
+        String[] string_array = new String[10];
+        int[] int_array = new int[10];
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < lines.length; i++) {
-            lines[i] = reader.readLine();
-            numbers[i] = lines[i].length();
+        for (int i = 0; i < string_array.length; i++) {
+            System.out.println("Input "+(i+1)+" line");
+            string_array[i] = reader.readLine();
+            int_array[i] = string_array[i].length();
         }
-        for (int element:numbers) {
+        for (int element:int_array) {
             System.out.println(element);
         }
     }
