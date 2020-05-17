c8cc7212c6af576e3a873b8f24f0366c7d38224e
Denis <davydovdmail@gmail.com>
2020-05-17 11:33:31 +0300

    Code editing

---
+++ src/main/java/programming2/typeConversion/Ex4.java
@@ -0,0 +1,20 @@
+package programming2.typeConversion;
+
+import java.util.Random;
+
+public class Ex4 {
+    /*Даны три переменные a, b и c. Изменить значения этих переменных так, чтобы в a хранилось значение a+b,
+   в b хранилась разность старых значений c−a,
+   а в c хранилось сумма старых значений a+b+c. Например, a=0, b=2, c=5, тогда новые значения a=2, b=5 и c=7.*/
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int a = rnd.nextInt((5-2)+5);
+        int b = rnd.nextInt((5-2)+5);
+        int c = rnd.nextInt((5-2)+5);
+        System.out.println("initial values: a = "+a+" b = "+b+" c = "+c);
+        a+=b;
+        b=c-(a-b);
+        c+=a;
+        System.out.println("final values: a = "+a+" b = "+b+" c = "+c);
+    }
+}
