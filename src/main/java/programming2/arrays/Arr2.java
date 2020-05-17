8570254f3d3f8137d05f9b309d63f631215c07ee
Denis <davydovdmail@gmail.com>
2020-05-17 17:32:41 +0300

    Code editing

--- src/main/java/programming2/arrays/Arr2.java
+++ src/main/java/programming2/arrays/Arr2.java
@@ -6,17 +6,19 @@ import java.io.InputStreamReader;
 
 public class Arr2 {
     /*Массив из строчек в обратном порядке
-    1. Создать массив на 10 строчек.
-    2. Ввести с клавиатуры 8 строчек и сохранить их в массив.
-    3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке.
-    Каждый элемент - с новой строки.*/
+1. Создать массив на 10 строчек.
+2. Ввести с клавиатуры 8 строчек и сохранить их в массив.
+3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке. Каждый элемент - с новой строки.*/
     public static void main(String[] args) throws IOException {
         String[] array = new String[10];
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < array.length - 2; i++) {
+        for (int i = 0; i < 8; i++) {
             array[i] = reader.readLine();
         }
-        for (int i = array.length-1; i >= 0; i--) {
+        outarray(array);
+    }
+    public static void outarray(String[] array){
+        for (int i = array.length-1; i >= 0 ; i--) {
             System.out.println(array[i]);
         }
     }
