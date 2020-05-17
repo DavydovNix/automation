e055804be3285a13b47c04efe0009d6f1435cf0e
Denis <davydovdmail@gmail.com>
2020-05-17 18:14:23 +0300

    Code editing

--- src/main/java/programming2/arrays/Arr4.java
+++ src/main/java/programming2/arrays/Arr4.java
@@ -4,23 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/*Массив из чисел в обратном порядке
-        1. Создать массив на 10 чисел.
-        2. Ввести с клавиатуры 10 чисел и записать их в массив.
-        3. Расположить элементы массива в обратном порядке.
-        4. Вывести результат на экран, каждое значение выводить с новой строки.*/
 public class Arr4 {
+/*Массив из чисел в обратном порядке
+1. Создать массив на 10 чисел.
+2. Ввести с клавиатуры 10 чисел и записать их в массив.
+3. Расположить элементы массива в обратном порядке.
+4. Вывести результат на экран, каждое значение выводить с новой строки.*/
     public static void main(String[] args) throws IOException {
         int[] array = new int[10];
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = array.length-1; i >= 0; i--) {
-            String s = reader.readLine();
-            array[i] = Integer.parseInt(s);
+        System.out.println("Input 10 arrays elements");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
         }
-        int buff;
-        for (int i : array) {
-            System.out.println(i);
+        int temp;
+        for (int i = 0; i < array.length/2; i++) {
+            temp = array[i];
+            //System.out.println(temp);
+            array[i] = array[array.length-i-1];
+            array[array.length-i-1]=temp;
         }
-
-    }
-}
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+}}
