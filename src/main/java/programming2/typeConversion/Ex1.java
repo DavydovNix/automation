e49cb46f7b9c29e6eff7d1c1209e9c687a708828
Denis <davydovdmail@gmail.com>
2020-05-17 08:39:10 +0300

    Code editing

--- src/main/java/programming2/typeConversion/Ex1.java
+++ src/main/java/programming2/typeConversion/Ex1.java
@@ -4,10 +4,11 @@ package programming2.typeConversion;
 * деления q на w с остатком. Пример вывода программы
 * (для случая, когда в q хранится 21, а в w хранится 8): 21 / 8 = 2 и 5 в остатке*/
 public class Ex1 {
+    public static final int q = 21;
+    public static final int w = 8;
     public static void main(String[] args) {
-        int q = 31,w = 8;
         if(q%w!=0)
-            System.out.println(q+"/"+w+" = "+q/w+" и "+q%w+" в остатке");
+            System.out.println(q+" / "+w+" = "+q/w+" и "+q%w+" в остатке");
         else System.out.println(q/w);
     }
 }
