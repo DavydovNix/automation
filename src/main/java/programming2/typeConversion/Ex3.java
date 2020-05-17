c35af935469dc73ab15d714f42609d2576df623c
Denis <davydovdmail@gmail.com>
2020-05-17 11:16:27 +0300

    Code editing

---
+++ src/main/java/programming2/typeConversion/Ex3.java
@@ -0,0 +1,14 @@
+package programming2.typeConversion;
+
+public class Ex3 {
+        /*В переменной n хранится вещественное число с ненулевой дробной частью.
+    Создайте программу, округляющую число n до ближайшего целого и выводящую результат на экран.*/
+
+    public static void rounding(){
+        double n = Math.random();
+        System.out.println("n = "+n+" "+"round n = "+Math.round(n));
+    }
+    public static void main(String[] args) {
+        rounding();
+    }
+}
