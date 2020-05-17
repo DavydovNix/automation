c3712d2cb9109e068fd9755b18f70dacd36adef4
Denis <davydovdmail@gmail.com>
2020-05-17 16:15:29 +0300

    Code editing

--- src/main/java/programming2/arrays/Arr1.java
+++ src/main/java/programming2/arrays/Arr1.java
@@ -5,33 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Arr1 {
-    /*Максимальное среди массива на 20 чисел
+   /*Максимальное среди массива на 20 чисел
 1. В методе initializeArray():
 1.1. Создайте массив на 20 чисел
 1.2. Считайте с консоли 20 чисел и заполните ими массив
 2. Метод max(int[] array) должен находить максимальное число из элементов массива*/
-    public static final int ARRAY_LENGTH = 20;
+
     public static void main(String[] args) throws IOException {
         int[] array = initializeArray();
         max(array);
     }
     public static int[] initializeArray() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int[] array = new int[ARRAY_LENGTH];
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            String s = reader.readLine();
-            array[i] = Integer.parseInt(s);
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Input element N: "+i);
+            array[i] = Integer.parseInt(reader.readLine());
         }
         return array;
     }
     public static void max(int[] array){
         int maxElement = array[0];
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            if (maxElement < array[i]){
+        for (int i = 0; i < array.length; i++) {
+            if(maxElement<array[i]){
                 maxElement = array[i];
             }
         }
         System.out.println();
-        System.out.println("max element = " + maxElement);
+        System.out.println("max array element = "+maxElement);
     }
 }
