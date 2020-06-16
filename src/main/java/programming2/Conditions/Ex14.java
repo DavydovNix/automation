1a8d13e7d044ad93d9974fe540d2ea6e6a95448d
Denis Davydov <davydovdmail@gmail.com>
2020-06-16 14:56:32 +0300

    Merge branch 'master' of D:\Java\automation with conflicts.

--- src/main/java/programming2/Conditions/Ex14.java
+++ src/main/java/programming2/Conditions/Ex14.java
@@ -5,15 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Ex14 {
+<<<<<<< Updated upstream
     /*Ввести с клавиатуры два числа m и n. Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
     Пример: m=2, n=4
             8888
             8888*/
+=======
+    /*Ввести с клавиатуры два числа m и n.
+Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
+Пример: m=2, n=4
+8888
+8888*/
+>>>>>>> Stashed changes
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int m = Integer.parseInt(reader.readLine());
         int n = Integer.parseInt(reader.readLine());
         for (int i = 0; i < m; i++) {
+<<<<<<< Updated upstream
+=======
+            //System.out.println(8);
+>>>>>>> Stashed changes
             for (int j = 0; j < n; j++) {
                 System.out.print(8);
             }
